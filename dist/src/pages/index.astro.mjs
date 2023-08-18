fa170433538b117208d7d47abae6bf4422f02d9a
darren christoper <darrenchristoper2500@gmail.com>
2023-08-18 17:28:55 -0500

    added build to production

---
+++ dist/src/pages/index.astro.mjs
@@ -0,0 +1,10 @@
+export { renderers } from '../../renderers.mjs';
+export { onRequest } from '../../_empty-middleware.mjs';
+import '../../chunks/astro.1d8372e8.mjs';
+import 'html-escaper';
+import 'react';
+import 'react-dom/server';
+
+const page = () => import('../../chunks/pages/index.astro.a56ed964.mjs');
+
+export { page };
